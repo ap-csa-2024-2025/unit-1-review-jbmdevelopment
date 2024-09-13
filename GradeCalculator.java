cf9bc74e8c41fa51eca08a3db0feb0a50b4ce5cd
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-09-13 11:16:32 -0700

    asagsdgsdsasasasfdsfgdsgsdgdsdsfin32fin32f23

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -10,12 +10,21 @@ public class GradeCalculator
     int fourthGrade = 25;
     double firstQuiz = 85.5;
     double secondQuiz = 95.3;
-    double finalExam = 50.25;
+    double finalExamGrade = 50.25;
     int hours = (int)((time / 60) + 0.5);
     double avgHWGrade = (double)(firstGrade + secondGrade + thirdGrade + fourthGrade)  / 4;
-    System.out.print(avgHWGrade);
+    double avgQuizGrade = (double)(firstQuiz + secondQuiz)  / 2;
+    double workWeight = 0.35;
+    double quizWeight = 0.15;
+    double finalExamWeight = 0.50;
+    // use modulo to get the minutes left from hours calculation (adjust it later)
+    int finalScore = (int)((avgHWGrade * workWeight) + (avgQuizGrade * quizWeight) + (finalExamGrade * finalExamWeight) + 0.5);
+    //System.out.print(finalScore);
+    //System.out.print(avgHWGrade);
+        //System.out.print(avgQuizGrade);
     System.out.println(course);
     System.out.println("Avg time per week in hours: " + hours);
     System.out.println("Avg time per week in minutes: " + time);
+    // print it out later...
   }
 }
