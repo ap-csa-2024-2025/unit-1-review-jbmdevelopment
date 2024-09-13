a9be855fe8b70b0cd23a4f2f2f5ca2c4a753c2d5
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-09-13 10:44:53 -0700

    checkpoint

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -2,6 +2,11 @@ public class GradeCalculator
 {
   public static void main(String[] args)
   {
-    // insert code here
+    String course = "AP Computer Science A";
+    int time = 250;
+    int firstGrade = 65;
+    int secondGrade = 70;
+    int thirdGrade = 95;
+    int fourthGrade = 25;
   }
 }
