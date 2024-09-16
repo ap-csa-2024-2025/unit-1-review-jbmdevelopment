386b492f8a19fff23ab3ccefa98a2cf57f2d7eba
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-09-16 10:48:12 -0700

    ez

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -11,20 +11,30 @@ public class GradeCalculator
     double firstQuiz = 85.5;
     double secondQuiz = 95.3;
     double finalExamGrade = 50.25;
-    int hours = (int)((time / 60) + 0.5);
+    int hours = (int)(time / 60);
+    int minutes = (time % 60);
     double avgHWGrade = (double)(firstGrade + secondGrade + thirdGrade + fourthGrade)  / 4;
     double avgQuizGrade = (double)(firstQuiz + secondQuiz)  / 2;
     double workWeight = 0.35;
     double quizWeight = 0.15;
     double finalExamWeight = 0.50;
-    // use modulo to get the minutes left from hours calculation (adjust it later)
     int finalScore = (int)((avgHWGrade * workWeight) + (avgQuizGrade * quizWeight) + (finalExamGrade * finalExamWeight) + 0.5);
-    //System.out.print(finalScore);
-    //System.out.print(avgHWGrade);
-        //System.out.print(avgQuizGrade);
-    System.out.println(course);
-    System.out.println("Avg time per week in hours: " + hours);
-    System.out.println("Avg time per week in minutes: " + time);
-    // print it out later...
+    System.out.println("Course name " + course);
+    System.out.println("Average time in minutes: " + time);
+    System.out.println("Homework grades for this course:");
+    System.out.println(firstGrade);
+    System.out.println(secondGrade);
+    System.out.println(thirdGrade);
+    System.out.println(fourthGrade);
+    System.out.println("Quiz grades for this class:");
+    System.out.println(firstQuiz);
+    System.out.println(secondQuiz);
+    System.out.println("Final exam grade for this course:");
+    System.out.println(finalExamGrade);
+    System.out.println("Weekly time spent: " + hours + " hours " + minutes + " minutes");
+    System.out.println("Average homework grade: " + avgHWGrade);
+    System.out.println("Average quiz grade: " + avgQuizGrade);
+    System.out.println("Final exam grade: " + finalExamGrade);
+    System.out.println("Overall grade: " + finalScore);
   }
 }
